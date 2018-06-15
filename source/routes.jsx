8e5c6b2e734de51accf419219be678e6b2ba5460
Thomas Christoffersen <t.christoffersen@creuna.no>
2018-06-15 12:41:05 +0200

    support for more pages

--- source/routes.jsx
+++ source/routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Home from './pages/home';
+import pages from './static/pages/pages';
 
 const Routes = () => (
   <Switch>
-    <Route exact path="/" component={Home} />
+    {pages.map(page => (
+      <Route key={page.url} path={page.url} component={page.component} />
+    ))}
   </Switch>
 );
 
